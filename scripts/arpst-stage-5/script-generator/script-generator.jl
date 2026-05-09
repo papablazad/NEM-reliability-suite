04c2b5cc294e07fc4941dd72252fce76da0bd749
Pablo Apablaza <papablaza@student.unimelb.edu.au>
2026-05-09 15:07:07 +1000

    updates to script generator

--- scripts/arpst-stage-5/script-generator/script-generator.jl
+++ scripts/arpst-stage-5/script-generator/script-generator.jl
@@ -33,19 +33,3 @@ generate_run_scripts(
     results_folder_name   = "results-spartan",
     sample_number_per_run = 100
 )
-
-# generate_run_scripts(
-#     target_years     = [2040],                          # e.g. collect(2025:2050)
-#     scenarios        = [2],
-#     poes             = [10],
-#     ref_traces       = [2011],
-#     samples          = 100,
-#     julia_output_dir = "/home/papablaza/git/NEM-reliability-suite/scripts/arpst-stage-5/jobs/runs-test",
-#     slurm_output_dir = "/home/papablaza/executions/ar-pst/jobs/test/slurm",
-#     batch_output_dir = "/home/papablaza/executions/ar-pst/jobs/test/batch",
-#     base_path        = "/data/gpfs/projects/punim2114/arpst/proj-4310_arpst_2026-1128.4.1597",
-#     julia_repo_path  = "/home/papablaza/git/NEM-reliability-suite",
-#     n_threads        = 10,
-#     time_limit       = "2-00:00:00",
-#     results_folder_name   = "results-100s",
-# )

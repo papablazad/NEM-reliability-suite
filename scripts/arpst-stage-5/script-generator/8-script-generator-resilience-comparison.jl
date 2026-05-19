5ac04735c289197358fe55e16b79b22dc8db1940
Pablo Apablaza <papablaza@student.unimelb.edu.au>
2026-05-19 17:45:24 +1000

    scripts from last week

--- scripts/arpst-stage-5/script-generator/8-script-generator-resilience-comparison.jl
+++ scripts/arpst-stage-5/script-generator/8-script-generator-resilience-comparison.jl
@@ -6,9 +6,9 @@ generate_run_scripts(
     poes                  = [50],
     ref_traces            = [2017],
     samples               = 500,
-    julia_output_dir      = "/home/papablaza/git/NEM-reliability-suite/scripts/arpst-stage-5/jobs/runs-r11-2017p10-s2-baseVPP-resilience",
-    slurm_output_dir      = "/home/papablaza/executions/ar-pst/jobs/runs-r11-2017p10-s2-baseVPP-resilience/slurm",
-    batch_output_dir      = "/home/papablaza/executions/ar-pst/jobs/runs-r11-2017p10-s2-baseVPP-resilience/batch",
+    julia_output_dir      = "/home/papablaza/git/NEM-reliability-suite/scripts/arpst-stage-5/jobs/runs-r11-2017p10-s2-baseVPP",
+    slurm_output_dir      = "/home/papablaza/executions/ar-pst/jobs/runs-r11-2017p10-s2-baseVPP/slurm",
+    batch_output_dir      = "/home/papablaza/executions/ar-pst/jobs/runs-r11-2017p10-s2-baseVPP/batch",
     base_path             = "/data/gpfs/projects/punim2114/arpst/proj-4310_arpst_2026-1128.4.1597",
     julia_repo_path       = "/home/papablaza/git/NEM-reliability-suite",
     n_threads             = 10,
@@ -16,8 +16,7 @@ generate_run_scripts(
     results_folder_name   = "results-spartan",
     sample_number_per_run = 100,
     assess_adequacy_kwargs = Dict(
-        :case_name      => "\"baseVPP\"",
         :DER_parameters => "PRASNEM.get_DER_parameters(; case=\"baseVPP\")",
-        :resilience_event => "\"heatwave-ref2017-ty2038\""
+        :case_name      => "\"baseVPP\""
     )
 )
